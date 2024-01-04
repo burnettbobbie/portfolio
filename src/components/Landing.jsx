2ab78be7139df71eee72ba53e74285fc7d18ce9c
burnettbobby <20298092@myclyde.ac.uk>
2024-01-04 14:41:16 +0000

    commit 34

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -9,13 +9,13 @@ import vid from '../assets/bobbie-tv.mp4';
 const Landing = () => {
   return (
     <>
-      <section className="flex items-center justify-center max-w-fit h-screen mx-auto relative pt-[80vh] xs:pt-[15vh] sm:pt-[10vh]  ">
+      <section className="flex items-center justify-center max-w-fit h-screen mx-auto relative pt-[80vh] xs:pt-[7vh] sm:pt-[10vh]  ">
         <motion.div initial={{ y: -450, opacity: 0, rotate:0}} animate={{ y: 0, opacity: 1, rotate: -15, transition: { delay:0.5, duration: 2, ease: easeInOut, type: "spring", bounce: 0.6  } }}  className="rounded-[15px] border-[#767676] border-[4px] hidden md:block scale-100 lg:scale-[1] mt-[-55vh] p-0 shadow-white shadow-[-15px_-15px_150px_10px] rotate-[-40deg] absolute">
           <video src={vid} loop autoPlay muted className="rounded-[15px] shadow-black shadow-xl bg-transparent border-black " alt="bobbie in tv">
           </video>
         </motion.div>
         <div className="text-white text-center ">
-          <div className="animation-container ">
+          <div className="animation-container sm:inline-block xs:hidden">
             <Lottie
               animationData={animationData}
               className="lottie-animation "
@@ -24,7 +24,7 @@ const Landing = () => {
             <p className="text-right Krona text-[20px]">Burnett</p>
           </div>
           <div  className="flex flex-col mt-[6vh] text-left mb-[40vh] xs:mb-[15vh] sm:mb-[0] mx-[15vw]  lg:leading-10">
-            <p className="z-10 text-[34px] Krona ">Junior Full Stack Developer </p>
+            <p className="z-10 text-[30px] Krona ">Junior Full Stack Developer </p>
             <div className="mt-6 rounded-[20px] shadow-slate-50 shadow-2xl ">
               <div className="bg-[rgba(255,255,255,0.2)] border-b-[10px] bg-gradient-to-br from-zinc-400 to-zinc-900 border-zinc-50 drop-shadow-xl shadow-[inset_8px_8px_20px_white] rounded-[20px] p-[35px] flex justify-evenly items-center">
                 <p className="text-white text-[20px] text-center font-light ">
