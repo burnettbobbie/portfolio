8e6d74c8962d55bbffe7d477b6149da7298f7e6e
burnettbobby <20298092@myclyde.ac.uk>
2024-01-08 16:18:24 +0000

    newLanding coomjit 24

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -26,7 +26,7 @@ const Landing = () => {
           <div  className="flex flex-col mt-[6vh] text-left mb-[40vh] xs:mb-[15vh] sm:mb-[0] mx-[15vw]  lg:leading-10">
             <p className="z-10 text-[30px] Krona ">Junior Full Stack Developer </p>
             <div className="mt-6 rounded-[20px] shadow-zinc-900  shadow-2xl">
-              <div className="bg-[rgba(208,207,207,0.2)] border-b-[3px] bg-gradient-to-bl from-sky-200 via-zinc-500 to-zinc-900 border-zinc-50 drop-shadow-xl shadow-[inset_8px_8px_20px_white] rounded-[20px] p-[35px] flex justify-evenly items-center">
+              <div className="bg-[rgba(208,207,207,0.2)] border-b-[3px] bg-gradient-to-bl from-blue-200 via-zinc-500 to-zinc-900 border-zinc-50 drop-shadow-xl shadow-[inset_8px_8px_20px_white] rounded-[20px] p-[35px] flex justify-evenly items-center">
                 <p className="text-white text-[16px] text-left Krona font-extralight ">
                 Taking a multidisciplinary approach, my objective is to build applications and websites that function effectively, bring enjoyment, and leave a lasting visual impact!
                 </p>
